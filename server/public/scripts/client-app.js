cd941a6adeba2be9ba8b9ff257f7a89723797e02
Jeff Kealy <jeffkealy@yahoo.com>
2016-11-22 13:28:02 -0600

    second commit

--- server/public/scripts/client-app.js
+++ server/public/scripts/client-app.js
@@ -1,4 +1,17 @@
 var myApp = angular.module("myApp", []);
+myApp.filter('unique', function() {
+
+ return function (arr, field) {
+   var o = {}, i, l = arr.length, r = [];
+   for(i=0; i<l;i+=1) {
+     o[arr[i][field]] = arr[i];
+   }
+   for(i in o) {
+     r.push(o[i]);
+   }
+   return r;
+ };
+})
 
 myApp.controller("BookController", ["$http", function($http) {
   console.log('running');
